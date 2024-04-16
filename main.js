b63e3c65a6d246f5f3bf77c0af905bcf49f9366e
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2024-04-15 23:00:40 -0700

    add speak, share, copy options

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-api = "https://api.quotable.io/random?maxLength=85";
+var api = "https://api.quotable.io/random?maxLength=85";
 var quote = document.getElementById("quote");
 var author = document.getElementById("author");
 
@@ -7,7 +7,46 @@ const getquote = async () => {
   const resp = await fetchapi.json();
   quote.innerText = resp.content;
   author.innerText = `---${resp.author}`;
-  console.log(resp);
 };
 
 getquote();
+
+// This function speak quote
+const speak = () => {
+  const synth = window.speechSynthesis;
+  const voice = new SpeechSynthesisUtterance(quote.innerHTML);
+  synth.speak(voice);
+};
+
+// this function share Tweet on twitter
+const tweet = () => {
+  window.open(
+    `https://twitter.com/intent/tweet?text=${quote.innerText}`,
+    "Tweet Window",
+    "width=600 , height=300"
+  );
+};
+
+// this function copy quote
+function copytext() {
+  // Create a range object
+  const range = document.createRange();
+  // Select the text inside the quote element
+  range.selectNodeContents(quote);
+
+  // Remove any existing selection
+  window.getSelection().removeAllRanges();
+  // Add the new range to the selection
+  window.getSelection().addRange(range);
+
+  try {
+    // Execute the copy command
+    document.execCommand("copy");
+    console.log("Text copied to clipboard!");
+  } catch (err) {
+    console.error("Failed to copy text to clipboard:", err);
+  }
+
+  // Clear the selection
+  window.getSelection().removeAllRanges();
+}
